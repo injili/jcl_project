c77fbcfa84dd62744127e58b620ab8e4db4e65af
francis-100 <mayiekaf@gmail.com>
2023-11-04 10:58:24 +0300

    completed back-end

--- django_ecommerce/store/admin.py
+++ django_ecommerce/store/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-
 from .models import *
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(ProductFeatures)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'user')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'digital', 'shop')
+
+@admin.register(ProductFeatures)
+class ProductFeaturesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'digital', 'size', 'weight', 'shop', 'color', 'sheets_in_ream', 'thickness')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'date_ordered', 'complete', 'transaction_id')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order', 'quantity', 'date_added')
+
+@admin.register(ShippingAddress)
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'order', 'address', 'city', 'street', 'phone_number', 'date_added')
